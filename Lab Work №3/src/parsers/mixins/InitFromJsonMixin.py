40091424ea804a384fd86603fc762f46ed4eee4a
yunas-x <113031087+yunas-x@users.noreply.github.com>
2023-12-06 19:11:57 +0500

    Update InitFromJsonMixin.py

--- Lab Work №3/src/parsers/mixins/InitFromJsonMixin.py
+++ Lab Work №3/src/parsers/mixins/InitFromJsonMixin.py
@@ -1,17 +1,19 @@
 import json
 from pathlib import Path
 
-class InitFromJson():
+class InitFromJson:
     
     @classmethod  
     def from_json(cls, path: Path, **kwargs):
         """Initiate object from file
+        The class must define the initializer 
+        where loaded object should be the first parameter
 
         Args:
             path (Path): path to Json
         """
         
         with open(path, "r") as file:
-            schema = json.load(file)
+            obj: dict = json.load(file)
         
-        return cls(schema=schema, kwargs)
+        return cls(obj, **kwargs)
