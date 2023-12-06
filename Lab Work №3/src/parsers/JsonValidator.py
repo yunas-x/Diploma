fe94fc5f6400e759bee00599d984e986a0cfe849
yunas-x <113031087+yunas-x@users.noreply.github.com>
2023-12-06 20:30:32 +0500

    Update JsonValidator.py

--- Lab Work №3/src/parsers/JsonValidator.py
+++ Lab Work №3/src/parsers/JsonValidator.py
@@ -7,12 +7,12 @@ class JsonValidator(ValidatorProtocol, InitFromFile):
     """Validates json file against the schema provided"""
     
     def __init__(self, schema: dict):
-        self.schema = schema
+        self._schema = schema
     
     def validate(self, data: dict, schema: dict=None) -> bool:
         
         if schema is None:
-            schema = self.schema
+            schema = self._schema
         
         validator = jsonschema.Draft7Validator(schema=schema)
         return validator.is_valid(data)
