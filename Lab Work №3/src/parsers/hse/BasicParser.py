17ba0d6d98d284a79c10c8c12580c7d58c25ec60
yunas-x <113031087+yunas-x@users.noreply.github.com>
2023-12-08 23:22:24 +0500

    Update BasicParser.py

--- Lab Work №3/src/parsers/hse/BasicParser.py
+++ Lab Work №3/src/parsers/hse/BasicParser.py
@@ -2,4 +2,9 @@ from parsers.protocols import ParserProtocol
 
 class BasicParser(ParserProtocol):
     """There will be HSE Basic Parser"""
-    pass
+
+    def parse(self, payload) -> dict:
+        pass
+
+    def __init__(self):
+        pass
