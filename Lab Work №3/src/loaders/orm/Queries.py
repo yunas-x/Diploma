73d8b8f6cc9bb5a3c8801b85caf3ae6e87798ffc
yunas-x <113031087+yunas-x@users.noreply.github.com>
2023-12-10 21:08:52 +0500

    Update Queries.py

--- Lab Work №3/src/loaders/orm/Queries.py
+++ Lab Work №3/src/loaders/orm/Queries.py
@@ -4,46 +4,47 @@ from sqlalchemy.orm import Session
 from loaders.models.BaseModel import BaseModel
 from loaders.orm.ORMStatus import ORMStatus
 
-def add(session: Session, entity: BaseModel) -> ORMStatus:
-    """Add row to DataBase
+class Queries:
 
-    Args:
-        session (Session): DataBase Session (might be injected through SessionMaker)
-        entity (BaseEntityModel): Entity to add
+    def __init__(self, session_maker: sessionmaker[Session]):
+        self._session_maker = session_maker
+    
+    def add(self, entity: BaseModel) -> ORMStatus:
+        """Add row to DataBase
+    
+        Args:
+            entity (BaseEntityModel): Entity to add
+    
+        Returns:
+            ORMStatus: OK/Fail
+        """
 
-    Returns:
-        ORMStatus: OK/Fail
-    """
-    try:
-        session.add(entity)
-        session.commit()
-        status = ORMStatus.OK
-    except:
-        status = ORMStatus.Fail
-        # There will be logging
-    finally:
-        session.close()
-        
-    return status
+        with self._session_maker() as session:
+            try:
+                session.add(entity)
+                session.commit()
+                status = ORMStatus.OK
+            except:
+                status = ORMStatus.Fail
+                # There will be logging
+        return status
+    
+    def add_all(self, entities: Iterable[BaseModel]) -> ORMStatus:
+        """Add rows to DataBase
+    
+        Args:
+            entities (Iterable[BaseEntityModel]): Entities to add
+    
+        Returns:
+            ORMStatus: OK/Fail
+        """
 
-def add_all(session: Session, entities: Iterable[BaseModel]) -> ORMStatus:
-    """Add rows to DataBase
-
-    Args:
-        session (Session): DataBase Session (might be injected through SessionMaker)
-        entities (Iterable[BaseEntityModel]): Entities to add
-
-    Returns:
-        ORMStatus: OK/Fail
-    """
-    try:
-        session.add_all(entities)
-        session.commit()
-        status = ORMStatus.OK
-    except:
-        status = ORMStatus.Fail
-        # There will be logging
-    finally:
-        session.close()
-        
-    return status
+        with self._session_maker() as session:
+            try:
+                session.add_all(entities)
+                session.commit()
+                status = ORMStatus.OK
+            except:
+                status = ORMStatus.Fail
+                # There will be logging
+        return status
