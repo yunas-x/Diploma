59292f5a3f38347d24ae92b6d94e778a03e589e2
yunas-x <113031087+yunas-x@users.noreply.github.com>
2023-12-10 21:12:18 +0500

    Update Queries.py

--- Lab Work №3/src/loaders/orm/Queries.py
+++ Lab Work №3/src/loaders/orm/Queries.py
@@ -6,6 +6,7 @@ from loaders.models.BaseModel import BaseModel
 from loaders.orm.ORMStatus import ORMStatus
 
 class Queries:
+    """There are DB-queries which are used for CRUD"""
 
     def __init__(self, session_maker: sessionmaker[Session]):
         self._session_maker = session_maker
