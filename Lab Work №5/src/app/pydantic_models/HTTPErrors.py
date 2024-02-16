07d85dd7663f9bc2bbe32601f7da4c71a87cb255
yunas-x <113031087+yunas-x@users.noreply.github.com>
2024-02-16 13:30:49 +0500

    Delete Lab Work №5 directory

--- Lab Work №5/src/app/pydantic_models/HTTPErrors.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class HTTPAuthError(BaseModel):
-    detail: str
-
-    class Config:
-        json_schema_extra = {
-            "example": {"detail": "HTTPException raised."},
-        }
